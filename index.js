17db459c43a9886496893c3fbea5216bb47d1147
Suresh Y <suresh.yadam0713@gmail.com>
2023-11-25 06:30:07 +0530

    updated code

--- index.js
+++ index.js
@@ -1,72 +1,97 @@
+require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: false,
-  },
-];
 const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.static("dist"));
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World</h1>");
-});
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
-});
-app.get("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const note = notes.find((note) => note.id === id);
-  console.log(note);
-  if (note) {
-    res.json(note);
-  } else {
-    // res.statusMessage = "Given record id is not availalbe in the server";
-    res.status(404).send("Given record id is not availalbe in the server");
-  }
-});
-app.delete("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  notes = notes.filter((note) => note.id !== id);
-  res.status(204).end();
-});
-
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((note) => note.id)) : 0;
-  return maxId + 1;
+const cors = require("cors");
+const Note = require("./models/notes");
+const requestLogger = (req, res, next) => {
+  console.log("Method", req.method);
+  console.log("Path", req.path);
+  console.log("Body", req.body);
+  console.log("----");
+  next();
 };
 
-app.post("/api/notes", (req, res) => {
-  const body = req.body;
-  if (!body.content) {
-    return res.status(400).json({
-      error: "content missing",
-    });
+const errorHandler = (err, req, res, next) => {
+  console.error("error", err.message);
+  if (err.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  } else if (err.name === "ValidationError") {
+    return res.status(404).json({ error: err.message });
   }
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    id: generateId(),
-  };
+  next(err);
+};
 
-  notes = notes.concat(note);
-  res.json(note);
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({
+    error: "unknown endpoint",
+  });
+};
+
+app.use(cors());
+app.use(express.json());
+app.use(requestLogger);
+app.use(express.static("dist"));
+
+app.get("/api/notes", (req, res) => {
+  Note.find({}).then((notes) => {
+    res.json(notes);
+  });
 });
 
-const PORT = process.env.PORT || 3001;
+app.get("/api/notes/:id", (req, res, next) => {
+  Note.findById(req.params.id)
+    .then((note) => {
+      if (note) {
+        res.json(note);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => {
+      console.log("error", err);
+      next(err);
+    });
+});
+app.delete("/api/notes/:id", (req, res, next) => {
+  Note.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((err) => next(err));
+});
+
+app.put("/api/notes/:id", (req, res, next) => {
+  const { content, important } = req.body;
+  console.log("content", content);
+  Note.findByIdAndUpdate(
+    req.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedNote) => {
+      res.json(updatedNote);
+    })
+    .catch((err) => next(err));
+});
+
+app.post("/api/notes", (req, res, next) => {
+  const body = req.body;
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
+  note
+    .save()
+    .then((savedNote) => {
+      res.json(savedNote);
+    })
+    .catch((err) => next(err));
+});
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
---
+++ mongo.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("Please send password as argument");
+  process.exit(1);
+}
+const password = process.argv[2];
+console.log("password", password);
+const url = `mongodb+srv://fullstacknotes:${password}@cluster0.vb4raga.mongodb.net/noteApp?retryWrites=true&w=majority`;
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+const Note = mongoose.model("Note", noteSchema);
+
+const note = new Note({
+  content: "I want to become fullstack developer",
+  important: true,
+});
+
+// note.save().then((result) => {
+//   console.log("Notes saved", result);
+//   mongoose.connection.close();
+// });
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
